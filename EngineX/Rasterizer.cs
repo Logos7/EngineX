82919b98d892f4137c6df29e85211c4ef4ea0642
Magdalena <magdalena.nitecka99@gmail.com>
2024-06-01 09:24:58 +0200

    wire triangle, filled triangle, horizontal line, clear

--- EngineX/Rasterizer.cs
+++ EngineX/Rasterizer.cs
@@ -29,9 +29,12 @@
             return b.GetPixel(x, y);
         }
 
-        public void HLine(int x, int y, int w)
+        public void HLine(int x, int y, int w, Color c)
         {
-
+            for (int i = x; i < x + w; i++)
+            {
+                SetPixel(i, y, c);
+            }
         }
 
         public void VLine(int x, int y, int h)
@@ -53,9 +56,12 @@
             cy = y;
         }
 
-        public void Clear(Color c)
+        public void Clear()
         {
-            // rś
+            for (int i = 0; i < b.Height; i++)
+            {
+                HLine(0, i, b.Width, Color.Black);
+            }
         }
 
         public void WiredDrawRectangle(int x1, int y1, int x2, int y2)
@@ -169,12 +175,99 @@
 
         public void WireTriangle(int x1, int y1, int x2 , int y2, int x3, int y3)
         {
-            // magdalena
+            Line(x1, y1, x2, y2);
+            Line(x2, y2, x3, y3);
+            Line(x3, y3, x1, y1);
         }
 
-        public void Triangle(int x1, int y1, int x2, int y2, int x3, int y3)
+        public void Triangle(int x1, int y1, int x2, int y2, int x3, int y3, Color c)
         {
-            // magdalena
+            SortVertices(ref x1, ref y1, ref x2, ref y2, ref x3, ref y3);
+
+            // Compute slopes
+            double dx1 = x2 == x1 ? 0 : (double)(x2 - x1) / (y2 - y1);
+            double dx2 = x3 == x1 ? 0 : (double)(x3 - x1) / (y3 - y1);
+            double dx3 = x3 == x2 ? 0 : (double)(x3 - x2) / (y3 - y2);
+
+            // Initial x values
+            double xs1 = x1;
+            double xs2 = x1;
+
+            // Fill the triangle
+            if (y2 == y3)
+            {
+                // Bottom flat triangle
+                for (int y = y1; y <= y3; y++)
+                {
+                    DrawScanline((int)xs1, (int)xs2, y, c);
+                    xs1 += dx1;
+                    xs2 += dx2;
+                }
+            }
+            else if (y1 == y2)
+            {
+                // Top flat triangle
+                xs1 = x2;
+                for (int y = y1; y <= y3; y++)
+                {
+                    DrawScanline((int)xs1, (int)xs2, y, c);
+                    xs1 += dx3;
+                    xs2 += dx2;
+                }
+            }
+            else
+            {
+                // General case
+                for (int y = y1; y <= y2; y++)
+                {
+                    DrawScanline((int)xs1, (int)xs2, y, c);
+                    xs1 += dx1;
+                    xs2 += dx2;
+                }
+
+                xs1 = x2;
+                for (int y = y2; y <= y3; y++)
+                {
+                    DrawScanline((int)xs1, (int)xs2, y, c);
+                    xs1 += dx3;
+                    xs2 += dx2;
+                }
+            }
+        }
+
+        private void SortVertices(ref int x1, ref int y1, ref int x2, ref int y2, ref int x3, ref int y3)
+        {
+            if (y1 > y2)
+            {
+                Swap(ref x1, ref x2);
+                Swap(ref y1, ref y2);
+            }
+
+            if (y1 > y3)
+            {
+                Swap(ref x1, ref x3);
+                Swap(ref y1, ref y3);
+            }
+
+            if (y2 > y3)
+            {
+                Swap(ref x2, ref x3);
+                Swap(ref y2, ref y3);
+            }
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            (a, b) = (b, a);
+        }
+
+        private void DrawScanline(int x1, int x2, int y, Color c)
+        {
+            if (x1 > x2) Swap(ref x1, ref x2);
+            for (int x = x1; x <= x2; x++)
+            {
+                SetPixel(x, y, c);
+            }
         }
     }
 }
