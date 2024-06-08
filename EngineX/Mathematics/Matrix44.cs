9a718add886f9dcab3d43d24ffb9d42e5dee65a4
John Doe <test@example.com>
2024-06-08 11:53:23 +0200

    It should work now (I hope)

---
+++ EngineX/Mathematics/Matrix44.cs
@@ -0,0 +1,70 @@
+﻿namespace EngineX.Mathematics
+{
+    public class Matrix44
+    {
+        public const byte size = 4;
+        private double[,] Rows = new double[size,size];
+
+        public Matrix44()
+        {
+            for (byte i = 0; i < size; i++)
+            {
+                for (byte j = 0; j < size; j++) Rows[i,j] = i == j? 1 : 0;
+            }
+        }
+
+        public Matrix44(double[,] array)
+        {
+            Rows = array;
+        }
+
+        public static Matrix44 operator +(Matrix44 m1, Matrix44 m2)
+        {
+            Matrix44 result = new();
+            for (byte i = 0; i < size; i++)
+            {
+                for (byte j = 0; j < size; j++) result.Rows[i,j] = m1.Rows[i,j] + m2.Rows[i,j];
+            }
+            return result;
+        }
+
+        public static Matrix44 operator -(Matrix44 m1, Matrix44 m2) => m1 + (-1 * m2);
+
+        public static Matrix44 operator *(double a, Matrix44 m)
+        {
+            Matrix44 result = new();
+            for (byte i = 0; i < size; i++)
+            {
+                for (byte j = 0; j < size; j++) result.Rows[i,j] = a * m.Rows[i,j];
+            }
+            return result;
+        }
+
+        public static Matrix44 operator *(Matrix44 m, double a) => a * m;
+
+        public Matrix44 T()
+        {
+            Matrix44 result = new();
+            for (byte i = 0; i < size; i++)
+            {
+                for (byte j = 0; j < size; j++) result.Rows[i,j] = Rows[j,i];
+            }
+            return result;
+        }
+
+        public static Matrix44 operator *(Matrix44 m1, Matrix44 m2)
+        {
+            Matrix44 result = new();
+            for (byte i = 0; i < size; i++)
+            {
+                for (byte j = 0; j < size; j++)
+                {
+                    double sum = 0;
+                    for (byte k = 0; k < size; k++) sum += m1.Rows[i,k] * m2.Rows[k,j];
+                    result.Rows[i,j] = sum;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ EngineX/Mathematics/Vector3.cs
@@ -0,0 +1,26 @@
+﻿namespace EngineX.Mathematics
+{
+    public class Vector3
+    {
+        public double X, Y, Z;
+
+        public Vector3()
+        {
+            X = Y = Z = 0;
+        }
+
+        public Vector3(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static Vector3 operator +(Vector3 v1, Vector3 v2) => new(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        public static Vector3 operator -(Vector3 v1, Vector3 v2) => new(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        public static Vector3 operator *(double a, Vector3 v) => new(a * v.X, a * v.Y, a * v.Z);
+        public static Vector3 operator *(Vector3 v, double a) => a * v;
+        public double Dot(Vector3 v) => X * v.X + Y * v.Y + Z * v.Z;
+        public double Length() => Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+    };
+}
