2f6b2bf45141e692c46445c5aafd9c92e2f94252
John Doe <test@example.com>
2024-06-08 07:42:44 +0200

    vector and matrix

--- EngineX/Mathematics/float3.cs
+++ EngineX/Mathematics/float3.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EngineX.Mathematics
+﻿namespace EngineX.Mathematics
 {
-    internal class float3
+    public class float3
     {
-    }
+        public double X, Y, Z;
+        public float3(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static float3 operator +(float3 v1, float3 v2) => new(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        public static float3 operator -(float3 v1, float3 v2) => new(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        public static float3 operator *(double a, float3 v) => new(a * v.X, a * v.Y, a * v.Z);
+        public static double dot(float3 v1, float3 v2) => v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
+    };
 }
