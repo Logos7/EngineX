0ce6c33ce6cd7439bc3c36ccc7a4c78ad53d64bf
John Doe <test@example.com>
2024-06-15 10:05:59 +0200

    matrix: matrix * vector, lookAt matrix, perspective vector:  normalize, cross

--- EngineX/Mathematics/Matrix44.cs
+++ EngineX/Mathematics/Matrix44.cs
@@ -3,7 +3,7 @@
     public class Matrix44
     {
         public const byte size = 4;
-        private double[,] Rows = new double[size,size];
+        private readonly double[,] Rows = new double[size,size];
 
         public Matrix44()
         {
@@ -65,6 +65,54 @@
                 }
             }
             return result;
+        }
+
+        public static Vector3 operator *(Matrix44 m, Vector3 v)
+        {
+            double[] array = new double[size], vector = new double[] { v.X, v.Y, v.Z, 1 };
+            for (byte i = 0; i < size; i++)
+            {
+                double sum = 0;
+                for (byte j = 0; j < size; j++) sum += m.Rows[i,j] * vector[j];
+                array[i] = sum;
+            }
+            return new(array[0], array[1], array[2]);
+        }
+
+        public static Matrix44 LookAt(Vector3 position, Vector3 target, Vector3 up)
+        {
+            /*
+             * Left-handed look-at matrix
+             * https://learn.microsoft.com/en-us/previous-versions/windows/desktop/bb281710(v=vs.85)
+            */
+            Vector3 z = (target - position).Normalize();
+            Vector3 x = up.Cross(z).Normalize();
+            Vector3 y = z.Cross(x);
+
+            double[,] array = new double[,] {
+                { x.X, y.X, z.X, 0 },
+                { x.Y, y.Y, z.Y, 0 },
+                { x.Z, y.Z, z.Z, 0 },
+                { -x.Dot(position), -y.Dot(position), -z.Dot(position), 1 }
+            };
+
+            return new(array);
+        }
+
+        public static Matrix44 Perspective(double width, double height, double near, double far)
+        {
+            /*
+             * Left-handed perspective projection matrix
+             * https://learn.microsoft.com/en-us/previous-versions/windows/desktop/bb281729(v=vs.85)
+            */
+            double[,] array = new double[,] {
+                { 2*near/width, 0, 0, 0 },
+                { 0, 2*near/height, 0, 0 },
+                { 0, 0, far/(far-near), 1 },
+                { 0, 0, near*far/(near-far), 0 }
+            };
+
+            return new(array);
         }
     }
 }
--- EngineX/Mathematics/Vector3.cs
+++ EngineX/Mathematics/Vector3.cs
@@ -21,6 +21,8 @@
         public static Vector3 operator *(double a, Vector3 v) => new(a * v.X, a * v.Y, a * v.Z);
         public static Vector3 operator *(Vector3 v, double a) => a * v;
         public double Dot(Vector3 v) => X * v.X + Y * v.Y + Z * v.Z;
-        public double Length() => Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+        public double Length() => Math.Sqrt((X * X) + (Y * Y) + (Z * Z));
+        public Vector3 Normalize() => this * (1 / Length());
+        public Vector3 Cross(Vector3 v) => new(Y * v.Z - Z * v.Y, Z * v.X - X * v.Z, X * v.Y - Y * v.X);
     };
 }
