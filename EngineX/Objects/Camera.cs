841ced421892e36000e1957d8bc05d19bb9696ca
John Doe <test@example.com>
2024-06-29 09:08:39 +0200

    Camera

--- EngineX/Objects/Camera.cs
+++ EngineX/Objects/Camera.cs
@@ -1,6 +1,18 @@
-﻿namespace EngineX.Objects
+﻿using System.Numerics;
+
+namespace EngineX.Objects
 {
-/*    public class Camera : Object3D
+    public class Camera : Object3D
     {
-    }*/
+        private readonly double near;
+        public Camera(string aName) : base(aName)
+        {
+            near = 50;
+        }
+
+        public (double X, double Y) Transform(Vector3 vector)
+        {
+            return ((vector.X + near) / vector.Z, (vector.Y + near) / vector.Z);
+        }
+    }
 }
