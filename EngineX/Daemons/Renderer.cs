d74c77b9bf1cd94a0ea1d9a40fc2f4834cf34417
Magdalena <magdalena.nitecka99@gmail.com>
2024-06-29 09:43:28 +0200

    renderer

--- EngineX/Daemons/Renderer.cs
+++ EngineX/Daemons/Renderer.cs
@@ -1,6 +1,21 @@
-﻿namespace EngineX.Daemons
+﻿using EngineX.Objects;
+
+namespace EngineX.Daemons
 {
     public class Renderer : ADaemon
     {
+        public void Render(Scene3D scene3D, Rasterizer rasterizer, Camera camera)
+        {
+            foreach (var object3D in scene3D._objects)
+            {
+                foreach (var triangle in object3D.Triangles)
+                {
+                    (int X, int Y) vertexA = camera.Transform(object3D.Vertices[triangle.a]);
+                    (int X, int Y) vertexB = camera.Transform(object3D.Vertices[triangle.b]);
+                    (int X, int Y) vertexC = camera.Transform(object3D.Vertices[triangle.c]);
+                    rasterizer.Triangle(vertexA.X, vertexA.Y, vertexB.X, vertexB.Y, vertexC.X, vertexC.Y, Color.Aqua);
+                }
+            }
+        }
     }
 }
